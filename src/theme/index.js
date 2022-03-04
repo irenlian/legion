a8b0c150bb1842d5ff128baf0b54f0ade759c01f
irenlian <irynalian@stockx.com>
2022-03-04 15:40:35 +0200

    Install theme

---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+import {createTheme} from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    // Name of the component
+    MuiButtonBase: {
+      defaultProps: {
+        // The props to change the default for.
+        disableRipple: true, // No more ripple!
+      },
+    },
+  },
+});
+
+export default theme;
